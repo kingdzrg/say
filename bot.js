144bf48442850d06ea16842e91747f4ed996f0c8
kingdzrg <58010386+kingdzrg@users.noreply.github.com>
2019-11-27 13:32:10 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,4 +13,41 @@ message.channel.send(args);
 };
 })
 
+client2.on('message', message => {
+if (message.content.startsWith(prefix + "say2")) {
+var args = message.content.split(" ")
+args = args.join(" ").slice(args[0].length);
+
+if (!args) return;
+
+message.channel.send(args);
+};
+})
+
+client3.on('message', message => {
+if (message.content.startsWith(prefix + "say3")) {
+var args = message.content.split(" ")
+args = args.join(" ").slice(args[0].length);
+
+if (!args) return;
+
+message.channel.send(args);
+};
+})
+
+client4.on('message', message => {
+if (message.content.startsWith(prefix + "say4")) {
+var args = message.content.split(" ")
+args = args.join(" ").slice(args[0].length);
+
+if (!args) return;
+
+message.channel.send(args);
+};
+})
+
 client.login(process.env.TOKEN);
+client2.login(process.env.TOKEN);
+client3.login(process.env.TOKEN);
+client4.login(process.env.TOKEN);
+
